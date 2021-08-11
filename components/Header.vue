5ca618dad59bff816d11d258152d359d0bd9d852
Riccardo Miccini <rimiccini@yahoo.it>
2021-08-11 03:05:25 +0200

    draft 3

--- components/Header.vue
+++ components/Header.vue
@@ -1,26 +1,19 @@
 <template>
   <div>
     <div class="font-robo w-full z-0 absolute">
-      <svg class="fill-current text-gray text-lg tracking-verywide stroke-current stroke-1" viewBox="0 0 61 12">
+      <svg class="fill-current text-gray text-lg tracking-verywide" viewBox="0 0 61 12">
         <text x="-0.75" y="12">B.SIDE</text>
       </svg>
     </div>
     <div class="p-8 lg:p-16 pb-0 w-full z-10 relative">
-      <div class="border-t border-r border-white pt-6 lg:pt-8 pr-6 lg:pr-8 flex flex-col lg:flex-row">
-        <h1 class="text-blue text-3xl font-piru w-full lg:w-48 mb-2 flex-grow">B.SIDE</h1>
+      <div class="border-t border-r border-white py-6 lg:py-8 pr-6 lg:pr-8">
+        <h1 class="text-blue text-xl font-piru w-full mb-2">B.SIDE</h1>
         <!-- <p class="tracking-wide font-sans text-justify w-48 flex-none">
           20 AUG 2021 <br>
           20.00 - 06.00 <br>
           Secret location <br>
           &nbsp;
         </p> -->
-        <ul class="tracking-mid lg:tracking-wide text-justify text-sm w-full md:w-5/12 flex-none">
-          <li>PERNILLE KROG MOGENSEN <a class="ml-2 hover:text-red text-blue" href="https://www.dr.dk/radio/p3/kollektivet-6/kollektivet-ambient-electronica-dubstep-og-noerdet-techno-20" target="_blank">[DRP3]</a></li>
-          <li>RMZ <a class="ml-2 hover:text-red text-blue" href="https://soundcloud.com/rmz-dnb" target="_blank">[CONCRETE JUNGLE]</a></li>
-          <li>DJ ROGER <a class="ml-2 hover:text-red text-blue" href="https://www.youtube.com/watch?v=u_4Cmzu-NBU" target="_blank">[BOLSJEN RADIO]</a></li>
-          <li>TEA LEWIS <a class="ml-2 hover:text-red text-blue" href="#" target="_blank">[TBA]</a></li>
-          <li>N.E.GIRL <a class="ml-2 hover:text-red text-blue" href="https://soundcloud.com/djnegirl" target="_blank">[SOLUMN RECORDS]</a></li>
-        </ul>
       </div>
     </div>
   </div>
--- components/Overlay.vue
+++ components/Overlay.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="mix-blend-difference w-full h-1/4 text-md bg-transparent bg-clip-padding backdrop-filter backdrop-blur-sm border-b border-t border-gray z-20 fixed bottom-0 lg:top-2/3">
-    <div class="w-full lg:w-2/3 mx-auto text-center lg:text-left h-full p-6 lg:p-0 lg:relative">
-      <p class="tracking-wide font-sans text-justify w-48 mx-auto lg:pl-2 lg:absolute lg:bottom-8 lg:left-1/2">
-        20 AUG 2021 <br>
-        20.00 - 06.00 <br>
-        Secret location
-      </p>
-      <span class="text-red font-piru lg:pr-2 lg:absolute lg:bottom-8 lg:left-16">
+  <div class="lg:mix-blend-difference w-full text-md bg-black bg-opacity-80 lg:bg-opacity-100 lg:bg-transparent bg-clip-padding backdrop-filter backdrop-blur-sm border-b border-t border-gray z-20">
+    <div class="w-full lg:w-2/3 mx-auto px-8 lg:px-24 py-8 flex gap-8">
+      <div class="flex-shrink lg:flex-1 order-last flex flex-col justify-center">
+        <p class="tracking-wide font-sans text-justify w-40 lg:w-48 text-sm lg:text-base">
+          20 AUG 2021 <br>
+          20.00 - 06.00 <br>
+          Secret location
+        </p>
+      </div>
+      <span class="text-red text-lg lg:text-xl font-piru flex-grow lg:flex-1 flex flex-col justify-center">
         <a href="#" target="_blank">buy tickets</a>
       </span>
     </div>
