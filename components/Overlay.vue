4bf40735e3432583e00cc28faaac96eaedfc20db
Riccardo Miccini <rimiccini@yahoo.it>
2021-08-12 02:24:05 +0200

    payment link

--- components/Overlay.vue
+++ components/Overlay.vue
@@ -8,8 +8,8 @@
           <span class="tracking-short md:tracking-wide">Secret location [CPH]</span>
         </p>
       </div>
-      <span class="text-red text-lg md:text-xl font-piru flex-grow md:flex-1 flex flex-col justify-center md:text-right drop-shadow-sm">
-        <a href="#" target="_blank">buy tickets&nbsp;</a>
+      <span class="text-red text-lg md:text-xl font-piru flex-grow md:flex-1 flex flex-col justify-center md:text-right hover:text-blue">
+        <a href="https://buy.stripe.com/eVag2kb562xY1Xy144" target="_blank">buy tickets&nbsp;</a>
       </span>
     </div>
   </div>
